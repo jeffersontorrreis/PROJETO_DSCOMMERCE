5ac37bd8b7ced768300fe98c14987105fc6de1cc
Jefferson Torres <jefferson.torrreis@gmail.com>
2025-12-13 21:28:38 -0300

    Refactor validation logic in AuthService

--- src/main/java/com/dscommerce/dscommerce/services/AuthService.java
+++ src/main/java/com/dscommerce/dscommerce/services/AuthService.java
@@ -12,8 +12,8 @@ public class AuthService {
     private UserService userService;
 
     public void validatelfOrAdmin(Long userId){
-        User me = userService.authenticated(); /*Pegamos o usuario auetenticado e mandamos ele para a varoavel "me"*/
-        if(!me.hasRole("ROLE_ADMIN") && !me.getId().equals(userId)){ /*Se o meu usuario não for admin e nem for esse "userId" que chegou como argumento lançamos a exception abaixo:*/
+        User me = userService.authenticated(); 
+        if(!me.hasRole("ROLE_ADMIN") && !me.getId().equals(userId)){ 
             throw new ForbiddenException("Access denied");
         }
     }
